28e4969c00eba161aae4e3615cd4ff0160985071
zbayle <126003179+zbayle@users.noreply.github.com>
2024-04-11 21:43:10 -0700

    Update package.json, App.css, and home_view.js

--- af-scanner/src/views/home_view.js
+++ af-scanner/src/views/home_view.js
@@ -5,8 +5,9 @@ class HomeView extends Component {
     render() {
         return (
             <div className='page_title'>
-            <img src={AirfiberLogo} alt="Airfiber Logo" className="logo" />
-            <h1>Inventory Systems</h1>
+                <img src={AirfiberLogo} alt="Airfiber Logo" className="logo" />
+                <h1>Inventory Systems</h1>
+            <img src={process.env.PUBLIC_URL + '/foblogo.jpg'} alt="Fob Logo" id="foblogo" />
             </div>
         );
     }
---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd C:\Users\AirFiber\Documents\GitHub\AFSystem && npm start"
+start cmd /k "cd C:\Users\AirFiber\Documents\GitHub\AFSystem\af-scanner && npm start"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --start-fullscreen http://localhost:3000
