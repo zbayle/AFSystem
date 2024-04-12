d9e6a71a92265cce6dcd49306fc16cf851307168
zbayle <126003179+zbayle@users.noreply.github.com>
2024-04-12 11:15:13 -0700

    Update HomeView component to use useContext hook and add fob logo conditionally

--- af-scanner/src/views/home_view.js
+++ af-scanner/src/views/home_view.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import AirfiberLogo from "../logo.svg";
+import { AuthContext } from '../components/Auth_comp';
 
-class HomeView extends Component {
-    render() {
-        return (
-            <div className='page_title'>
-                <img src={AirfiberLogo} alt="Airfiber Logo" className="logo" />
-                <h1>Inventory Systems</h1>
-            <img src={process.env.PUBLIC_URL + '/foblogo.jpg'} alt="Fob Logo" id="foblogo" />
-            </div>
-        );
-    }
+const HomeView = () => {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    return (
+        <div className='page_title'>
+            <img src={AirfiberLogo} alt="Airfiber Logo" className="logo" />
+            <h1>Inventory Systems</h1>
+            {!isAuthenticated && <img src={process.env.PUBLIC_URL + '/foblogo.jpg'} alt="Fob Logo" id="foblogo" />}
+        </div>
+    );
 }
 
 export default HomeView;
